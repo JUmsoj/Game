982b1ee9d998537a70ed27a9282f3c1dda5a1f89
LEMMEJIT <qamarvn@gmail.com>
2024-12-29 21:19:05 -0500

    Bug Fixes

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ function Enter_Code() {
   
     
     
+  
   Log_Code();
- 
   if(trys > 4 && code != actual_code) {
     for(let i = 0; i < actual_code.length; i++) {
       code3.removeChild(code3.firstChild);
     }
-    alert(`Wrong,the correct code  was ${actual_code} ` );
+    alert(`Wrong,the correct code  was ${actual_code}`);
     sto = true
     setTimeout(function() {
     location.reload()
@@ -55,18 +55,67 @@ function randomize(size) {
     }
    
     code_history.style.gridTemplateColumns = `repeat(${actual_code.length}, auto)`;
+    console.log(actual_code)
 }
-
+function CharactersInStr(str, char) {
+  let thing = 0;
+  for(let i = 0; i < 5; i++) {
+    if(str[i] === char) {
+      thing++
+    }
+  }
+  return thing
+}
 function Log_Code() {
+  
+    let map = {}
+    let correct_ints = {}
+    let chars = []
+    let elems = []
     for(let i = 0; i < code.length; i++) {
+      if(map[code[i]] === undefined) {
+        map[code[i]]=0
+      }
+      
+    
+
       const code2=code_history.appendChild(document.createElement("div"));
       code2.textContent = code[i];
-      code2.style.backgroundColor = actual_code[i] === code[i] ? "green" : "red";
-      if(code2.style.backgroundColor === "red" && actual_code.includes(code[i])) {
-        code2.style.backgroundColor = "yellow";
+      elems.push(code2)
+        code2.style.backgroundColor = actual_code[i] === code[i] ? "green" : "red";
+        if(code2.style.backgroundColor === "green") {
+          if(correct_ints[code[i]] === undefined) {
+            correct_ints[code[i]] = 0
+          }
+          if(!chars.includes(code[i])) {
+            chars.push(code[i])
+          }
+          console.warn(chars[chars.length-1])
+          correct_ints[code[i]]++;
+        }
+        if(code2.style.backgroundColor === "red" && actual_code.includes(code[i]) && map[code[i]] < CharactersInStr(actual_code, code[i])) {
+          code2.style.backgroundColor = "yellow";
+        }
+        map[code[i]]+=1
+        console.log(map[code[i]])
+    }
+    for(let i = 0; i < chars.length; i++) {
+      if(correct_ints[chars[i]] === CharactersInStr(actual_code, chars[i])) {
+        console.log("Match")
+        for(let j = 0; j < elems.length; j++) {
+          const obj = elems[j]
+          const color = getComputedStyle(obj).getPropertyValue("background-color")
+          const green = "rgb(255, 255, 0)"
+          console.log(color)
+          if(color === green && obj.textContent === chars[i]) {
+            obj.style.backgroundColor = "red"
+            console.log("Purged")
+          }
+        }
       }
     }
 }
+
 for(let i = 0; i < 10; i++) {
    const x = create_element("div", grid, i);
    x.addEventListener("click",function() {
